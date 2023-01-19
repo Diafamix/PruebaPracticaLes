5860d53f391adc94621e5ca9c21e9860b5c534b9
DESKTOP-BR53GKA\Carlos <carloswow8@gmail.com>
2023-01-20 00:54:21 +0100

    PrimerComit

---
+++ src/main/java/com/practica/les/practicaLes/PracticaLesApplication.java
@@ -0,0 +1,13 @@
+package com.practica.les.practicaLes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaLesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaLesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practica/les/practicaLes/PracticaLesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica.les.practicaLes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaLesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
