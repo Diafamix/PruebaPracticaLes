3efedabc8f6035ff21b341d7523f404b1ccdfd05
DESKTOP-BR53GKA\Carlos <carloswow8@gmail.com>
2023-01-24 00:42:56 +0100

    Creacion de la entidad Profesor

--- src/main/java/com/practica/les/practicaLes/Data/Entities/AlumnosModel.java
+++ src/main/java/com/practica/les/practicaLes/Data/Entities/AlumnosModel.java
@@ -1,9 +1,17 @@
 package com.practica.les.practicaLes.Data.Entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Controller;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+// Para cambiar el nombre de la tabla en la base de datos
+@Table(name = "ALUMNOS")
 public class AlumnosModel {
 
     @Id
@@ -11,45 +19,7 @@ public class AlumnosModel {
     private String nombre;
     private String apellidos;
 
-    public AlumnosModel() {
-    }
+    @ManyToOne
+    private  ProfesorModel profesorModel;
 
-    public AlumnosModel(int id, String nombre, String apellidos) {
-        this.id = id;
-        this.nombre = nombre;
-        this.apellidos = apellidos;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellidos() {
-        return apellidos;
-    }
-
-    public void setApellidos(String apellidos) {
-        this.apellidos = apellidos;
-    }
-
-    @Override
-    public String toString() {
-        return "Alumnos{" +
-                "id=" + id +
-                ", nombre='" + nombre + '\'' +
-                ", apellidos='" + apellidos + '\'' +
-                '}';
-    }
 }
---
+++ src/main/java/com/practica/les/practicaLes/Data/Entities/ProfesorModel.java
@@ -0,0 +1,24 @@
+package com.practica.les.practicaLes.Data.Entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//Crea la PERSISTENCIA -> Lo mete en la base de datos
+@Entity
+//Crear los get set y constructores
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name = "PROFESOR")
+public class ProfesorModel {
+
+    @Id
+    public long id;
+    public String nombre;
+    public String apellido;
+
+}
